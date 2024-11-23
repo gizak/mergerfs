877d02f12e51e6cdb1ead07067002f2ac6073e1a
Antonio SJ Musumeci <trapexit@spawn.link>
2024-11-23 01:03:22 -0600

    func_getattr_ff.cpp

--- src/func_getattr_ff.cpp
+++ src/func_getattr_ff.cpp
@@ -1,10 +1,10 @@
 #include "func_getattr_ff.hpp"
 
 int
-Func2::process(const Branches  &branches_,
-               const fs::Path  &fusepath_,
-               struct stat     *st_,
-               fuse_timeouts_t *timeout_)
+Func2::GetattrFF::process(const Branches  &branches_,
+                          const fs::Path  &fusepath_,
+                          struct stat     *st_,
+                          fuse_timeouts_t *timeout_)
 {
   return -ENOENT;
 }
