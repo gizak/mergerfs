6dc9b411691503cbd9631335522aeba2e2dcea3c
Antonio SJ Musumeci <trapexit@spawn.link>
2025-04-25 18:30:44 -0500

    option_parser.cpp

--- src/option_parser.cpp
+++ src/option_parser.cpp
@@ -483,7 +483,7 @@ namespace options
         is_dir = ghc::filesystem::is_directory(i->path,ec);
         if(!is_dir)
           {
-            i = cfg->branches->erase(i);
+            i = cfg->branches()->erase(i);
             continue;
           }
 
